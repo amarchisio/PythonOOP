fb8a64abf7c1017ba7ce4942d15f6eb577e2ed3d
amarchisio <andrewmarchisio@gmail.com>
2016-12-24 15:07:37 -0500

    starting classVariables

---
+++ ClassVariables.py
@@ -0,0 +1,16 @@
+#Python Object-Oriented Programming
+
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('Jim', 'Smith', 50000)
+emp_2 = Employee('John', 'Doe', 60000)
